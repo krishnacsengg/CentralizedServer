1a36722bce685a5bf2331f1c9583f89dda28ef59
krishna <krishcsengg@gmail.com>
2021-06-16 21:37:09 +0530

    Initial commit

---
+++ src/main/java/com/cloud/cofigs/CentralizedServer/CentralizedServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cofigs.CentralizedServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/cofigs/CentralizedServer/CentralizedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cofigs.CentralizedServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
